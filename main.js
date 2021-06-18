0eada250669a593439ab7875c56f3333a9303536
Isaac Heist <isaacheist@gmail.com>
2021-06-17 21:08:10 -0700

    set up initial gh-pages branch

--- main.js
+++ main.js
@@ -1,109 +1,87 @@
+"use strict";
 // console.log('File is linked')
-function randomId(): string {
+function randomId() {
     const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
     return uint32.toString(16);
 }
-
 class Player {
-    readonly id: string;
-    readonly name: string;
-    private scores: number[];
-    private totalScore: number;
-
-    constructor(name: string) {
+    constructor(name) {
         this.id = 'player' + randomId();
         this.name = name;
         this.scores = [];
         this.totalScore = 0;
     }
-
-    addRoundPoints(points: number): void {
-        if(this.totalScore === 0 && points < 1000 && points !== 0) {
+    addRoundPoints(points) {
+        if (this.totalScore === 0 && points < 1000 && points !== 0) {
             this.scores.push(0);
             return;
         }
-
-        if(this.totalScore + points > Game.winningScore) {
+        if (this.totalScore + points > Game.winningScore) {
             this.scores.push(0);
             throw new Error('Over20000');
         }
-
         this.scores.push(points);
         this.totalScore = this.scores.reduce((acc, curr) => acc + curr);
-
-        if(this.totalScore === Game.winningScore) {
+        if (this.totalScore === Game.winningScore) {
             handleWin(this);
         }
     }
-
-    getScoresArray(): string[] {
+    getScoresArray() {
         return this.scores.map(score => score.toString());
     }
-
-    getTotalScore(): number {
+    getTotalScore() {
         return this.totalScore;
     }
 }
-
 class Game {
-    readonly id: string;
-    readonly players: Player[];
-    private currentPlayIndex: number;
-    activeGame: boolean;
-    static winningScore: number = 20000;
-
     constructor() {
         this.id = 'game' + randomId();
         this.players = [];
         this.currentPlayIndex = 0;
         this.activeGame = true;
     }
-
-    addPlayer(player: Player) {
+    addPlayer(player) {
         this.players.push(player);
     }
-
     getCurrentPlayer() {
         return this.players[this.currentPlayIndex];
     }
-
     nextTurn() {
         const totalPlayers = this.players.length;
         this.currentPlayIndex += 1;
-        if(this.currentPlayIndex >= totalPlayers) {
+        if (this.currentPlayIndex >= totalPlayers) {
             this.currentPlayIndex = 0;
         }
     }
 }
-
-const loginSection = <HTMLElement>document.getElementById('login');
-const addusersSection = <HTMLElement>document.getElementById('addusers');
-const gameplaySection = <HTMLElement>document.getElementById('gameplay');
-const gameOverSection = <HTMLElement>document.getElementById('gameOver');
-const totalScore = <HTMLHeadingElement>document.getElementById('totalScore');
-const scoreInput = <HTMLInputElement>document.getElementById('roundScore');
-const playerInput = <HTMLInputElement>document.getElementById('playername');
-const playersList = <HTMLOListElement>document.getElementById('listOfPlayers');
-const addPlayerButton = <HTMLButtonElement>document.getElementById('addplayer');
-const addToScoreButton = <HTMLButtonElement>document.getElementById('addToScore');
-const prevScoresList = <HTMLOListElement>document.getElementById('previousScores');
-const currentPlayerTitle = <HTMLHeadingElement>document.getElementById('currentPlayer');
-const gameWarning = <HTMLHeadingElement>document.getElementById('gameWarning');
-const endTitle = <HTMLHeadingElement>document.getElementById('endTitle');
-const endScores = <HTMLHeadingElement>document.getElementById('endScores');
-const reloadPageButton = <HTMLHeadingElement>document.getElementById('reloadPage');
-
-let game: Game;
-
-function startGame(e: Event) {
-    if(e.target != null) {
-        const selected = <HTMLElement>e.target;
-        if(selected.innerText === 'Start Game') {
-            if(addusersSection.hidden) {
+Game.winningScore = 20000;
+const loginSection = document.getElementById('login');
+const addusersSection = document.getElementById('addusers');
+const gameplaySection = document.getElementById('gameplay');
+const gameOverSection = document.getElementById('gameOver');
+const totalScore = document.getElementById('totalScore');
+const scoreInput = document.getElementById('roundScore');
+const playerInput = document.getElementById('playername');
+const playersList = document.getElementById('listOfPlayers');
+const addPlayerButton = document.getElementById('addplayer');
+const addToScoreButton = document.getElementById('addToScore');
+const prevScoresList = document.getElementById('previousScores');
+const currentPlayerTitle = document.getElementById('currentPlayer');
+const gameWarning = document.getElementById('gameWarning');
+const endTitle = document.getElementById('endTitle');
+const endScores = document.getElementById('endScores');
+const reloadPageButton = document.getElementById('reloadPage');
+let game;
+function startGame(e) {
+    if (e.target != null) {
+        const selected = e.target;
+        if (selected.innerText === 'Start Game') {
+            if (addusersSection.hidden) {
                 loginSection.hidden = true;
                 addusersSection.hidden = false;
                 game = new Game();
-            } else if(!addusersSection.hidden && game.players.length !== 0){
+            }
+            else if (!addusersSection.hidden && game.players.length !== 0) {
                 addusersSection.hidden = true;
                 gameplaySection.hidden = false;
                 currentPlayerTitle.innerText = `${game.getCurrentPlayer().name}'s turn`;
@@ -111,8 +89,8 @@ function startGame(e: Event) {
             }
         }
     }
-};
-
+}
+;
 function handleAddPlayerClick() {
     const playerNameInput = playerInput.value.trim();
     playerInput.value = '';
@@ -122,9 +100,9 @@ function handleAddPlayerClick() {
         const playerListItem = document.createElement('li');
         playerListItem.appendChild(document.createTextNode(element.name));
         playersList.appendChild(playerListItem);
-    })
-};
-
+    });
+}
+;
 function handleAddToScoreClick() {
     const numberInput = parseInt(scoreInput.value);
     let error = false;
@@ -133,24 +111,25 @@ function handleAddToScoreClick() {
         prevScoresList.innerHTML = '';
         try {
             game.getCurrentPlayer().addRoundPoints(numberInput); //Need to check for win
-        } catch (err) {
+        }
+        catch (err) {
             error = true;
-            if(err.message === 'Over20000') {
+            if (err.message === 'Over20000') {
                 const lastPlayer = game.getCurrentPlayer().name;
                 gameWarning.innerText = `${lastPlayer}'s total score went over 20,000 so their last score was zero`;
             }
         }
-        
         game.nextTurn();
         const currentPlayer = game.getCurrentPlayer();
         const playersTotalScore = currentPlayer.getTotalScore();
-        if(currentPlayer.getTotalScore() === 0) {
+        if (currentPlayer.getTotalScore() === 0) {
             error = true;
             gameWarning.innerText = `You must score at least 1000 points to get on the board, otherwise your score is 0 for this round.`;
         }
-        if(!error) {
+        if (!error) {
             gameWarning.style.display = 'none';
-        } else {
+        }
+        else {
             gameWarning.style.display = 'inherit';
         }
         currentPlayerTitle.innerText = `${currentPlayer.name}'s turn`;
@@ -161,9 +140,9 @@ function handleAddToScoreClick() {
             prevScoresList.appendChild(scoreListItem);
         });
     }
-};
-
-function handleWin(winningPlayer: Player) {
+}
+;
+function handleWin(winningPlayer) {
     console.log(`${winningPlayer.name} has won the game!!!`);
     gameplaySection.hidden = true;
     gameOverSection.hidden = false;
@@ -180,11 +159,9 @@ function handleWin(winningPlayer: Player) {
     });
     game.activeGame = false;
 }
-
 function playAgain() {
     window.location.reload();
 }
-
 loginSection.addEventListener('click', e => startGame(e));
 addusersSection.addEventListener('click', e => startGame(e));
 addToScoreButton.addEventListener('click', handleAddToScoreClick);
