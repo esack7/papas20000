71538fb4dad7f792cf65677c1548ccd74f46f829
Isaac Heist <isaacheist@gmail.com>
2021-06-28 22:04:02 -0700

    updated script to implement game persistence

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict";
-// console.log('File is linked')
 function randomId() {
     const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
     return uint32.toString(16);
 }
 class Player {
-    constructor(name) {
-        this.id = 'player' + randomId();
+    constructor(name, id = 'player' + randomId(), scores = [], totalScore = 0) {
+        this.id = id;
         this.name = name;
-        this.scores = [];
-        this.totalScore = 0;
+        this.scores = scores;
+        this.totalScore = totalScore;
     }
     addRoundPoints(points) {
         if (this.totalScore === 0 && points < 1000 && points !== 0) {
@@ -37,13 +36,27 @@ class Player {
     }
 }
 class Game {
-    constructor() {
-        this.id = 'game' + randomId();
-        this.players = [];
-        this.winnersBracket = [];
-        this.currentPlayIndex = 0;
-        this.activeGame = true;
-        this.lastRound = false;
+    constructor(id = `game${randomId()}`, players = [], winnersBracket = [], currentPlayIndex = 0, activeGame = true, lastRound = false, gameWarningText = '', errorState = false) {
+        this.id = id;
+        this.winnersBracket = winnersBracket;
+        this.currentPlayIndex = currentPlayIndex;
+        this.activeGame = activeGame;
+        this.lastRound = lastRound;
+        this.gameWarningText = gameWarningText;
+        this.errorState = errorState;
+        if (players.length === 0) {
+            this.players = players;
+        }
+        else {
+            this.players = players.map(player => {
+                if (player.scores.length === 0) {
+                    return new Player(player.name, player.id, player.scores, 0);
+                }
+                else {
+                    return new Player(player.name, player.id, player.scores, player.scores.reduce((acc, curr) => acc + curr));
+                }
+            });
+        }
     }
     addPlayer(player) {
         this.players.push(player);
@@ -93,8 +106,8 @@ function startGame(e) {
             else if (!addusersSection.hidden && game.players.length !== 0) {
                 addusersSection.hidden = true;
                 gameplaySection.hidden = false;
-                currentPlayerTitle.innerText = `${game.getCurrentPlayer().name}'s turn`;
-                totalScore.innerText = '0';
+                localStorage.setItem('recentGame', JSON.stringify(game));
+                handlePostScoreDisplay();
             }
         }
     }
@@ -116,7 +129,7 @@ function handleAddPlayerClick() {
 ;
 function handleAddToScoreClick() {
     const numberInput = parseInt(scoreInput.value);
-    let error = false;
+    game.errorState = false;
     scoreInput.value = '';
     if (!isNaN(numberInput)) {
         prevScoresList.innerHTML = '';
@@ -125,60 +138,70 @@ function handleAddToScoreClick() {
             game.getCurrentPlayer().addRoundPoints(numberInput);
         }
         catch (err) {
-            error = true;
+            game.errorState = true;
             if (err.message === 'Over20000') {
-                gameWarning.innerText = `${lastPlayer.name}'s total score went over 20,000 so their last score was zero`;
+                game.gameWarningText = `${lastPlayer.name}'s total score went over 20,000 so their last score was zero`;
             }
             if (err.message === 'Not multiple of 50') {
-                gameWarning.innerText = 'The score must be a multiple of 50.';
-                gameWarning.style.display = 'inherit';
+                game.gameWarningText = 'The score must be a multiple of 50.';
+                localStorage.setItem('recentGame', JSON.stringify(game));
+                handlePostScoreDisplay();
                 return;
             }
         }
         game.nextTurn();
-        const currentPlayer = game.getCurrentPlayer();
-        if (game.lastRound && game.winnersBracket.includes(currentPlayer.name)) {
-            if (game.winnersBracket.length === 1) {
-                handleWin(currentPlayer);
-            }
-            else {
-                handleShowdown(game);
-            }
-        }
-        else {
-            const playersTotalScore = currentPlayer.getTotalScore();
-            const lastPlayerScores = lastPlayer.getScoresArray();
-            if (playersTotalScore === 0) {
-                error = true;
-                gameWarning.innerText = `You must score at least 1000 points to get on the board, otherwise your score is 0 for this round.`;
-            }
-            if (!error) {
-                gameWarning.style.display = 'none';
-            }
-            else {
-                gameWarning.style.display = 'inherit';
-            }
-            if (lastPlayerScores.length > 0 && playersTotalScore !== 0) {
-                previousScore.innerText = `${lastPlayer.name}'s score was ${lastPlayerScores[lastPlayerScores.length - 1]}`;
-                previousScore.hidden = false;
-            }
-            currentPlayerTitle.innerText = `${currentPlayer.name}'s turn`;
-            totalScore.innerText = playersTotalScore.toString();
-            currentPlayer.getScoresArray().forEach(score => {
-                const scoreListItem = document.createElement('li');
-                scoreListItem.appendChild(document.createTextNode(score));
-                prevScoresList.appendChild(scoreListItem);
-            });
-        }
+        localStorage.setItem('recentGame', JSON.stringify(game));
+        handlePostScoreDisplay();
     }
 }
 ;
+function handlePostScoreDisplay() {
+    const currentPlayer = game.getCurrentPlayer();
+    gameWarning.hidden = true;
+    let lastPlayer;
+    if (game.currentPlayIndex === 0) {
+        lastPlayer = game.players[game.players.length - 1];
+    }
+    else {
+        lastPlayer = game.players[game.currentPlayIndex - 1];
+    }
+    if (game.lastRound && game.winnersBracket.includes(currentPlayer.name)) {
+        if (game.winnersBracket.length === 1) {
+            handleWin(currentPlayer);
+        }
+        else {
+            handleShowdown(game);
+        }
+    }
+    else {
+        const playersTotalScore = currentPlayer.getTotalScore();
+        const lastPlayerScores = lastPlayer.getScoresArray();
+        if (playersTotalScore === 0) {
+            game.errorState = true;
+            game.gameWarningText = `You must score at least 1000 points to get on the board, otherwise your score is 0 for this round.`;
+        }
+        if (game.errorState) {
+            gameWarning.innerText = game.gameWarningText;
+            gameWarning.hidden = false;
+        }
+        if (lastPlayerScores.length > 0 && playersTotalScore !== 0) {
+            previousScore.innerText = `${lastPlayer.name}'s score was ${lastPlayerScores[lastPlayerScores.length - 1]}`;
+            previousScore.hidden = false;
+        }
+        currentPlayerTitle.innerText = `${currentPlayer.name}'s turn`;
+        totalScore.innerText = playersTotalScore.toString();
+        currentPlayer.getScoresArray().forEach(score => {
+            const scoreListItem = document.createElement('li');
+            scoreListItem.appendChild(document.createTextNode(score));
+            prevScoresList.appendChild(scoreListItem);
+        });
+    }
+}
 function playLastRound(playerWhoReachedTwentyThousand) {
     game.lastRound = true;
     game.addToWinnersBracket(playerWhoReachedTwentyThousand);
 }
 function handleWin(winningPlayer) {
-    console.log(`${winningPlayer.name} has won the game!!!`);
     gameplaySection.hidden = true;
     gameOverSection.hidden = false;
     endTitle.innerText = `${winningPlayer.name} has won the game!!!`;
@@ -193,9 +216,9 @@ function handleWin(winningPlayer) {
         endScores.appendChild(playerRow);
     });
     game.activeGame = false;
+    localStorage.setItem('recentGame', JSON.stringify(game));
 }
 function handleShowdown(game) {
-    console.log(`Game Showdown!!!`);
     gameplaySection.hidden = true;
     gameOverSection.hidden = false;
     let listOfShowdownPlayers = '\n';
@@ -214,10 +237,26 @@ function handleShowdown(game) {
         endScores.appendChild(playerRow);
     });
     game.activeGame = false;
+    localStorage.setItem('recentGame', JSON.stringify(game));
 }
 function playAgain() {
     window.location.reload();
 }
+function loadGame(gameState) {
+    game = new Game(gameState.id, gameState.players, gameState.winnersBracket, gameState.currentPlayIndex, gameState.activeGame, gameState.lastRound, gameState.gameWarningText, gameState.errorState);
+    loginSection.hidden = true;
+    addusersSection.hidden = true;
+    gameplaySection.hidden = false;
+    handlePostScoreDisplay();
+}
+window.onload = () => {
+    if (localStorage.recentGame) {
+        const gameState = JSON.parse(localStorage.recentGame);
+        if (gameState.activeGame) {
+            loadGame(gameState);
+        }
+    }
+};
 loginSection.addEventListener('click', e => startGame(e));
 addusersSection.addEventListener('click', e => startGame(e));
 addToScoreButton.addEventListener('click', handleAddToScoreClick);
